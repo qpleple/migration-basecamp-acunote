455fcc23d898af820307c8af86f784a54ff1f94e
Quentin Pleplé <quentin.pleple@gmail.com>
2011-06-06 21:13:25 -0700

    Added param file

---
+++ params.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Configuration file for the migration script of todo lists from Basecamp to Acunote
+
+# URL for API calls
+url = "https://example.basecamphq.com"
+
+# API key
+apikey = "04f69569a36d306701238466d5ea12cb097b959d"
+
+# Users
+# List here all the user IDs in Basecamp and associate a name in Acunote :
+# Basecamp IDs => Acunote names
+creatorNames = {
+    '7248751' : "quentin",
+    '1122334' : "bob",
+}
